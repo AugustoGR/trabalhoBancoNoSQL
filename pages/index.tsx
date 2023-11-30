db91d551afe9dd976717c15881840bfb79ca60b4
Augusto Garcia da Rosa <augusto.garciadarosa@gmail.com>
2023-11-30 14:47:21 -0300

    feat: configuração da parte prática

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,8 @@ export const getServerSideProps: GetServerSideProps<
   ConnectionStatus
 > = async () => {
   try {
-    await clientPromise
+    const client = await clientPromise
+    const db = client.db('pessoasData')
     // `await clientPromise` will use the default database passed in the MONGODB_URI
     // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
     //
@@ -19,7 +20,8 @@ export const getServerSideProps: GetServerSideProps<
     //
     // Then you can execute queries against your database like so:
     // db.find({}) or any of the MongoDB Node Driver commands
-
+    const result = await db.collection('pessoas').find({}).toArray()
+    console.log(result)
     return {
       props: { isConnected: true },
     }
